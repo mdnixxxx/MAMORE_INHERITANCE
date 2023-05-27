06843547dcea95984df8c67c056fbec1c8f82a5a
mdnixxxx <131971442+mdnixxxx@users.noreply.github.com>
2023-05-27 19:35:00 +0800

    U_2_Spy_Plane.java

--- U_2_Spy_Plane.java
+++ U_2_Spy_Plane.java
@@ -7,4 +7,9 @@ public class U_2_Spy_Plane extends ToyotaVIOS{
 	void fly() {
 		System.out.println("The plane is flying.");
 	}
+	
+	@Override
+	void stop() {
+		System.out.println("The plane landed safely.");
+	}
 }
